4bb6ddae8f85b7b96b45e4f7c6b5d02a5014ee2b
Brian Young <bdy-github@hiddentower.com>
2013-05-31 10:51:27 -0700

    - Refactored to Modernized Objective-C

--- src/KalViewController.m
+++ src/KalViewController.m
@@ -133,7 +133,7 @@ extern const CGSize kTileSize;
   NSArray *markedDates = [theDataSource markedDatesFrom:logic.fromDate to:logic.toDate];
   NSMutableArray *dates = [markedDates mutableCopy];
   for (int i=0; i<[dates count]; i++)
-    [dates replaceObjectAtIndex:i withObject:[KalDate dateFromNSDate:[dates objectAtIndex:i]]];
+    dates[i] = [KalDate dateFromNSDate:dates[i]];
   
   [[self calendarView] markTilesForDates:dates];
   [self didSelectDate:self.calendarView.selectedDate];
